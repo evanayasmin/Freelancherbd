748d21ffc162eee3eb4c509ab3140c4d25dbc7ae
evanayasmin <evanay4@gmail.com>
2025-08-31 14:08:17 +0600

    ModelView add

--- src/main/java/com/evanadev/freelancherbd/controller/AuthController.java
+++ src/main/java/com/evanadev/freelancherbd/controller/AuthController.java
@@ -6,32 +6,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.Date;
 
-@Controller
+@RestController
 public class AuthController {
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/register")
-    public String register() {
-        return "register";
+    public ModelAndView  register() {
+
+        return new ModelAndView("register");
     }
 
     @PostMapping("/register")
-    public String registerUser(@RequestParam String fname, String lname, String username, String email, String password, String country, String city, String address, String phone, String role, String gender, Integer status, Date birthday) {
+    public ModelAndView  registerUser(@RequestParam String fname, String lname, String username, String email, String password, String country, String city, String address, String phone, String role, String gender, Integer status, Date birthday) {
         userService.RegisterUser(fname,  lname,  username,  email,  password,  country,  city,
                  address,  phone,  role,  gender,  status,  birthday);
-        return "redirect:/login";
+
+        return new ModelAndView("login");
     }
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public ModelAndView login() {
+        return new ModelAndView("login");
     }
 
     @GetMapping("/home")
-    public String home() {
-        return "home";
+    public ModelAndView home() {
+         return new ModelAndView("home");
     }
 }
