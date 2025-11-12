f8b44ffd3eb9a7c47cec4d636fb60f02c7a5b668
evanayasmin <evanay4@gmail.com>
2025-11-12 11:54:06 +0600

    recommended freelancers list

---
+++ src/main/java/com/evanadev/freelancherbd/controller/FreelancerController.java
@@ -0,0 +1,73 @@
+package com.evanadev.freelancherbd.controller;
+
+import com.evanadev.freelancherbd.model.CustomUserDetail;
+import com.evanadev.freelancherbd.model.Job;
+import com.evanadev.freelancherbd.model.JobStatus;
+import com.evanadev.freelancherbd.model.User;
+import com.evanadev.freelancherbd.service.FreelancerService;
+import com.evanadev.freelancherbd.service.JobService;
+import com.evanadev.freelancherbd.service.UserService;
+import com.evanadev.freelancherbd.util.AESUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class FreelancerController {
+
+    @Autowired
+    UserService userService;
+
+    JobService jobService;
+    AESUtil aesUtil;
+
+    private static final Logger log = LoggerFactory.getLogger(FreelancerController.class);
+    @Autowired
+    private FreelancerService freelancerService;
+
+    public FreelancerController(UserService userService, JobService jobService, AESUtil aesUtil ) {
+        this.userService = userService;
+        this.jobService = jobService;
+    }
+
+
+    @GetMapping("/skilled_freelancer")
+    public String GetSkilledFreelancer(Model model){
+
+        return "skilled_freelancer";
+    }
+
+    @GetMapping("/toprated_freelancer")
+    public String GetTopFreelancer(Model model){
+
+        return "top_rated_freelancer";
+    }
+
+    @GetMapping("/employer/recommended_freelancer")
+    public String GetRecommendedFreelancer(@ModelAttribute("loggedUser") CustomUserDetail loggedUser, Model model){
+
+        log.info("Loggedin user=",loggedUser.getUser().getUsername());
+        Long userId = loggedUser.getId();
+        Map<Job, List<User>> recommendedMap =
+                freelancerService.findByRecommendedFreelancer(userId);
+
+        // Flatten all recommended freelancers (avoid duplicates)
+        List<User> recommendedFreelancers = recommendedMap.values().stream()
+                .flatMap(List::stream)
+                .distinct()
+                .toList();
+
+        model.addAttribute("freelancers", recommendedFreelancers);
+        model.addAttribute("aesUtil", aesUtil);
+        return "recommended_freelancers";
+    }
+
+}
--- src/main/java/com/evanadev/freelancherbd/repository/JobRepository.java
+++ src/main/java/com/evanadev/freelancherbd/repository/JobRepository.java
@@ -37,6 +37,9 @@ public interface JobRepository extends JpaRepository<Job, Long> {
     @Query("SELECT DISTINCT j FROM Job j JOIN FETCH j.category WHERE j.jobStatus = :jobStatus")
     List<Job> findByRecommendedJobStatus(@Param("jobStatus") JobStatus jobStatus);
 
+    @Query("SELECT DISTINCT j FROM Job j JOIN FETCH j.category WHERE j.user.id = :userId")
+    List<Job> findByRecommendedFreelancer(@Param("userId") Long userId);
+
     @Query("SELECT DISTINCT jb FROM JobTraffic jt " +
             "JOIN jt.job jb " +
             "JOIN FETCH jb.category " +
